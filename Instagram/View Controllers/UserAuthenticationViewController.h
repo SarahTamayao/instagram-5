60b661e0c7ba131401e5770eda884ca69b905773
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-07-03 19:00:41 -0500

    completed login AutoLayout

---
+++ Instagram/View Controllers/UserAuthenticationViewController.h
@@ -0,0 +1,16 @@
+//
+//  UserAuthenticationViewController.h
+//  Instagram
+//
+//  Created by Rigre Reinier Garciandia Larquin on 7/3/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserAuthenticationViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/View Controllers/UserAuthenticationViewController.m
@@ -0,0 +1,53 @@
+//
+//  UserAuthenticationViewController.m
+//  Instagram
+//
+//  Created by Rigre Reinier Garciandia Larquin on 7/3/21.
+//
+
+#import "UserAuthenticationViewController.h"
+#import <Parse/Parse.h>
+
+@interface UserAuthenticationViewController ()
+
+@end
+
+@implementation UserAuthenticationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)registerUser {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    newUser.email = self.emailField.text;
+    newUser.password = self.passwordField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+        }
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
