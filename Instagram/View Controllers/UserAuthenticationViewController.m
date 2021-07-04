51460cc9f4046df3e0739a07e36068d592659fd9
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-07-03 19:21:37 -0500

    got basic login and signup

--- Instagram/View Controllers/UserAuthenticationViewController.m
+++ Instagram/View Controllers/UserAuthenticationViewController.m
@@ -9,6 +9,8 @@
 #import <Parse/Parse.h>
 
 @interface UserAuthenticationViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -19,13 +21,30 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)didTapSignUp:(UIButton *)sender {
+    if(self.usernameField.text.length > 0 && self.passwordField.text.length > 0){
+        [self registerUser];
+    }
+    else {
+        // TODO: Display Empty Field Error Message
+    }
+}
+
+- (IBAction)didTapLogin:(UIButton *)sender {
+    if(self.usernameField.text.length > 0 && self.passwordField.text.length > 0){
+        [self loginUser];
+    }
+    else {
+        // TODO: Display Empty Field Error Message
+    }
+}
+
 - (void)registerUser {
     // initialize a user object
     PFUser *newUser = [PFUser user];
     
     // set user properties
     newUser.username = self.usernameField.text;
-    newUser.email = self.emailField.text;
     newUser.password = self.passwordField.text;
     
     // call sign up function on the object
@@ -34,8 +53,25 @@
             NSLog(@"Error: %@", error.localizedDescription);
         } else {
             NSLog(@"User registered successfully");
-            
+        
             // manually segue to logged in view
+            [self performSegueWithIdentifier:@"login" sender:self];
+        }
+    }];
+}
+
+- (void)loginUser {
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            // display view controller that needs to shown after successful login
+            [self performSegueWithIdentifier:@"login" sender:self];
         }
     }];
 }
