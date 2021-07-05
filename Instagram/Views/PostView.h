593bac01b2eec3b8e8bc7b9377115942e97ccb62
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-07-05 18:47:05 -0500

    created post nib layout

---
+++ Instagram/Views/PostView.h
@@ -0,0 +1,16 @@
+//
+//  PostView.h
+//  Instagram
+//
+//  Created by Rigre Reinier Garciandia Larquin on 7/5/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PostView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
