b334d3844ba8168158bbeea024a5d234f49ad262
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-07-09 16:01:10 -0500

    removed old code from default image picker

--- Instagram/View Controllers/ComposeViewController.m
+++ Instagram/View Controllers/ComposeViewController.m
@@ -12,7 +12,7 @@
 #import <MBProgressHUD/MBProgressHUD.h>
 #import "CustomCameraViewController.h"
 
-@interface ComposeViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@interface ComposeViewController () <UINavigationControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *imagePreview;
 @property (weak, nonatomic) IBOutlet UITextView *captionTextView;
 @property (weak, nonatomic) IBOutlet UIView *imagePlaceHolderView;
@@ -23,12 +23,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-//    [self launchImagePicker];
     
-    [self performSegueWithIdentifier:@"cameraSegue" sender:self];
     [self setupGestures];
     [self styleCaptionTextView];
+    [self performSegueWithIdentifier:@"cameraSegue" sender:self];
 }
 
 - (void)styleCaptionTextView {
@@ -49,45 +47,12 @@
 
 - (void)didTapImage: (UITapGestureRecognizer *)sender {
     NSLog(@"tapped on image");
-    [self launchImagePicker];
-}
-
-- (void)launchImagePicker {
-    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
-    imagePickerVC.delegate = self;
-    imagePickerVC.allowsEditing = YES;
-
-    // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
-    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
-    }
-    else {
-        NSLog(@"Camera 🚫 available so we will use photo library instead");
-        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-    }
-
-    [self presentViewController:imagePickerVC animated:YES completion:nil];
-}
-
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
-    
-    self.imagePlaceHolderView.hidden = true;
-    
-    // Get the image captured by the UIImagePickerController
-    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
-    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
-
-    // Do something with the images (based on your use case)
-    self.imagePreview.image = editedImage;
-    
-    // Dismiss UIImagePickerController to go back to your original view controller
-    [self dismissViewControllerAnimated:YES completion:^(){
-        [self.captionTextView becomeFirstResponder];
-    }];
+    [self performSegueWithIdentifier:@"cameraSegue" sender:self];
 }
 
 - (void)didFinishPickingMediaWithImage:(UIImage *)image {
     self.imagePreview.image = image;
+    [self.captionTextView becomeFirstResponder];
 }
 
 - (IBAction)didTapCancel:(UIBarButtonItem *)sender {
